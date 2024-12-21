a4b60c0e6f7ec78cf80b39b5ac6a5e1ff63ea9a8
DIANDRA NAUFAL ABROR <151877527+DiandraNA@users.noreply.github.com>
2024-12-21 23:48:13 +0700

    index.vue

---
+++ Frontend/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar :user="user" />
+    <div class="text-center">
+      <h1>Selamat Datang di Website Peminjaman Barang P26</h1>
+      <p>Please login to proceed.</p>
+      <router-link to="/login">
+        <button>Login</button>
+      </router-link>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Navbar from '~/components/Navbar.vue';
+import Footer from '~/components/Footer.vue';
+
+const user = null;
+</script>
